5f3dc8e767107134666a3edcf90edaa1609defce
Joao <j.pedro_raulino@outlook.com>
2023-03-25 19:02:54 -0300

    Teste Front-end

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// const dropdownButton = document.getElementById('dropdown-button')
-// const dropdownContent = document.getElementById('dropdown-content')
+const dropdownButton = document.getElementById('dropdown-button')
+const dropdownContent = document.getElementById('dropdown-content')
 
-// dropdownButton.addEventListener('click', function () {
-//   dropdownContent.classList.toggle('show')
-// })
+dropdownButton.addEventListener('click', function () {
+  dropdownContent.classList.toggle('show')
+})
