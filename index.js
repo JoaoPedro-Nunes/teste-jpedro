28263181dca708a496b01a20883bc0976f6d79f5
Joao <j.pedro_raulino@outlook.com>
2023-03-25 14:56:05 -0300

    main

---
+++ index.js
@@ -0,0 +1,6 @@
+// const dropdownButton = document.getElementById('dropdown-button')
+// const dropdownContent = document.getElementById('dropdown-content')
+
+// dropdownButton.addEventListener('click', function () {
+//   dropdownContent.classList.toggle('show')
+// })
