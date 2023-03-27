a1c34d2e70165fc89dc6aed3fd34103b9784e2cd
Joao <j.pedro_raulino@outlook.com>
2023-03-26 23:52:01 -0300

    main

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const dropdownButton = document.getElementById('dropdown-button')
-const dropdownContent = document.getElementById('dropdown-content')
+function mostrarcard() {
+  var mostrarcard = document.getElementById('mostrarcard')
+  if (mostrarcard.style.display === 'none') {
+    mostrarcard.style.display = 'block'
+    setTimeout(() => {
+      mostrarcard.classList.add('transicao')
+    }, 10)
+  } else {
+    mostrarcard.classList.remove('transicao')
+    setTimeout(() => {
+      mostrarcard.style.display = 'none'
+    }, 500)
+  }
+}
 
-dropdownButton.addEventListener('click', function () {
-  dropdownContent.classList.toggle('show')
+const expandButton = document.querySelector('.seta')
+const menu = document.querySelector('.menu')
+const collapsedList = document.querySelector('.collapsed-list')
+const expandedList = document.querySelector('.expanded-list')
+expandButton.addEventListener('click', () => {
+  menu.classList.toggle('expanded')
+  collapsedList.style.display = menu.classList.contains('expanded')
+    ? 'none'
+    : 'block'
+  expandedList.style.display = menu.classList.contains('expanded')
+    ? 'block'
+    : 'none'
 })
